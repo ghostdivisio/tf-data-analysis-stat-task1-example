c2eb837363732f3680a89e9e0a7852aeca8d1e52
ghostdivisio <112753558+ghostdivisio@users.noreply.github.com>
2023-04-05 15:24:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1326113898 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    b=list(range(len(x)))
+    c=[]
+    for x1, x2 in zip(x, b):
+        c.append(x1*x2)
+        d=sum(c)/sum(x)
+    return d
