04a5b7d29a2180df57721c7d4d19c9119090c4bd
ghostdivisio <112753558+ghostdivisio@users.noreply.github.com>
2023-04-07 13:55:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,14 +5,11 @@ import numpy as np
 chat_id = 1326113898 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    b=list(range(len(x)))
-    c=[]
-    for x1, x2 in zip(x, b):
-        c.append(x1*x2)
-        if len(x)==10:
-            d=(sum(c)/sum(x))*0.98
-        if len(x)==100:
-            d=(sum(c)/sum(x))*0.81
-        if len(x)==1000:
-            d=sum(c)/sum(x)*0.8012
-    return d
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    #lmbd = np.mean(x) / 48
+    #return lmbd # Ваш ответ
+    total_lamps = np.sum(x)
+    lambda_ = total_lamps / (len(x) * 48)
+    return lambda_
